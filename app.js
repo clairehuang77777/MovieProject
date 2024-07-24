862de9c40e2e1014b117657953a14ff743155f7e
huang <clairehuang77777@gmail.com>
2024-07-24 16:46:17 +0800

    built router setup & controller setup

--- app.js
+++ app.js
@@ -2,8 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//設定路由
 app.get('/', (req , res) => {
-  res.send('express app for movies')
+  res.redirect('/movies')
+})
+
+//靜態路由
+app.get('/movies',(req , res) =>{
+  res.send('listing movies')
+})
+
+//動態路由
+app.get('/movies/:id',(req , res)=>{
+  const id = req.params.id
+  res.send(`read movie:${id}`)
 })
 
 app.listen(port, () => {
