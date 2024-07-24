9f6c4779d12ce74659d28befa6b08a00cbb155d4
huang <clairehuang77777@gmail.com>
2024-07-24 22:48:11 +0800

    bulid:use express-handler as view engine

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
+//載入express
 const express = require('express')
+const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
+
+//載入handlebars
+app.engine('.hbs', engine({extname: '.hbs'})) //設置新的模板引擎
+app.set('view engine', '.hbs') //使用.hbs作為默認引擎
+app.set('views', './views') //設置模板文件的目錄
 
 //設定靜態文件資源
 app.use(express.static('public'))
@@ -12,7 +19,7 @@ app.get('/', (req , res) => {
 
 //靜態路由
 app.get('/movies',(req , res) =>{
-  res.send('listing movies')
+  res.render('index')
 })
 
 //動態路由
@@ -21,6 +28,7 @@ app.get('/movies/:id',(req , res)=>{
   res.send(`read movie:${id}`)
 })
 
+//監聽port是否被trigger
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
 })
